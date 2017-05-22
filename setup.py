05d0d6a18b4a8551e4e7076c536bd1880a49b1cd
murch <ryan.c.murchie@gmail.com>
2017-05-22 00:06:07 -0400

    add setup.py for install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='datatypes',
+    version='0.1.0',
+    packages=['datatypes'],
+    url='',
+    license='',
+    author='Ryan',
+    author_email='',
+    description='Data type classes'
+)
