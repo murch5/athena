5baff71c5e10bcc7bf7dd6b960882489f4799c86
murch <ryan.c.murchie@gmail.com>
2017-06-07 23:02:06 -0400

    update to use process_manager class add logging removed old func; clean-up

--- data_manager/datatypes/data.py
+++ data_manager/datatypes/data.py
@@ -1,29 +1,25 @@
 import os as os
 
+import logging as logging
+logger = logging.getLogger(__name__)
 
 class Data:
-    def __init__(self):
-        self.name = None
-        self.filename = None
-        self.file_ext = None
-        self.data = None
-        self.processing_XML = None
-
-        return
 
     def load(self):
         return
 
-    def processing(self, processing_XML):
-        self.processing_XML = processing_XML
+    def evaluate_process_stack(self):
 
-        for process in self.processing_XML:
-            self.process(process)
-
-    processing_type = {}
+        self.data = self.process_manager.pass_thru_stack(self.data)
+        logger.debug("------ Processed data shape: " + str(self.data.shape))
+        pass
 
     def get(self):
         return self.data
+
+    def set_process_manager(self, process_manager):
+        self.process_manager = process_manager
+        pass
 
     def set_filename(self, filename):
         self.filename = filename
@@ -35,47 +31,15 @@ class Data:
     def get_name(self):
         return self.name
 
-    def process(self, process_XML):
-        process_type = process_XML.tag
-        self.processing_type[process_type](self, process_XML)
+    def __init__(self):
+        self.name = None
+        self.filename = None
+        self.file_ext = None
+        self.data = None
+        self.processing_stack = None
+        self.type = None
 
+        self.processing_type = {}
+
+        self.process_manager = None
         return
-
-    def parse_process(self, process_type, process_XML):
-        self.processing_type[process_type](self, process_XML)
-        return
-
-    @staticmethod
-    def checkXML(process_XML, xml):
-        check = False
-        if process_XML.find(xml) is not None:
-            check = True
-        else:
-            check = False
-
-        return check
-
-    @staticmethod
-    def getXMLvalue(process_XML, xml):
-        data = process_XML.find(xml)
-        data_type = data.attrib["data_type"]
-        value = None
-        if data_type in ["int", "i"]:
-            value = int(data.text)
-        elif data_type in ["float", "f"]:
-            value = float(data.text)
-        elif data_type in ["bool", "b"]:
-            value = bool(data.text)
-        elif data_type in ["str", "s"]:
-            value = str(data.text)
-        elif data_type in ["list_index", "li"]:
-            value = [int(x) for x in data.text.split(",")]
-        elif data_type in ["range_index", "ri"]:
-            value = [int(x) for x in data.text.split("-")]
-        elif data_type in ["axis_name", "axis"]:
-            value = [str(x) for x in data.text.split(",")]
-
-        else:
-            value = str(data.text)
-
-        return value
