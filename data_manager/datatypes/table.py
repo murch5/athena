b5db7ae68c458c8f5fc98fa162afa1e63178b558
murch <ryan.c.murchie@gmail.com>
2017-06-11 19:29:30 -0400

    change filename attr reference to file_name clean up old functions

--- data_manager/datatypes/table.py
+++ data_manager/datatypes/table.py
@@ -11,51 +11,15 @@ class Table(Data):
     def load(self):
 
         if self.file_ext == "csv":
-            self.data = pd.read_csv(self.filename, sep=",",header=0)
+            self.data = pd.read_csv(self.file_name, sep=",",header=0)
 
 
         elif self.file_ext == "tsv":
-            self.data = pd.read_table(self.filename)
+            self.data = pd.read_table(self.file_name)
         elif self.file_ext == "xls" or self.file_ext == "xlsx":
-            self.data = pd.read_excel(self.filename)
+            self.data = pd.read_excel(self.file_name)
 
         return
-
-    def filter(self, process_settings):
-
-        return
-
-    def extract(self, process_settings):
-        return
-
-    def subset(self, process_settings):
-
-        axis = 0
-        subset = 0
-
-        if process_settings.get("axis"):
-            axis = process_settings.get("axis")
-        if process_settings.get("set"):
-            subset = process_settings.get("set")
-
-        if axis in ["col", "c"]:
-            self.data = self.data.ix[subset]
-        elif axis in ["row", "r"]:
-            self.data = self.data.ix[:, subset]
-
-        return
-
-    def groupby_value(self, process_settings):
-
-        delim_flag = "False"
-        if process_settings.get("delim"):
-            delim_flag = process_settings.get("delim")
-
-        if delim_flag == "True":
-            self.data = self.data.str.split(";").apply(pd.Series, 1).stack()
-
-        self.data = self.data.groupby(self.data.iloc[:, 0]).size()
-        self.data.columns = ["Index", "GroupedCounts"]
 
     def get_index_labels(self, axis):
         labels = []
