c76b4be7e3272ec7e1d8b8bc69d59a3f5071645f
murch <ryan.c.murchie@gmail.com>
2017-06-11 19:29:56 -0400

    fix object accessor func in data manager

--- data_manager/data_manager.py
+++ data_manager/data_manager.py
@@ -4,31 +4,14 @@ import factory_manager as fm
 
 class DataManager(fm.FactoryStack):
 
-    def add_data(self, data):
-        self.data_list.append(data)
-        self.data_name_dict.update({data.get_name(): len(self.data_list) - 1})
-        return
-
     def get(self):
-        return self.data_list[0].get()
-
-    def get_data_obj(self):
-        return self.data_list[0]
-
-    def get_byname(self, name):
-        return self.data_list[self.data_name_dict[name]].get()
-
-    def get_data_obj_byname(self, name):
-        return self.data_list[self.data_name_dict[name]]
+        return self.obj_list[0].get("data")
 
     def map_value(self, value, data_map):
         search = "key==" + str(value)
         val = data_map.query(search)
         return val
         # return data_map.query(value)[1]
-
-    def get_data_dict(self):
-        return self.data_name_dict
 
     def map_transform(self, data_in, data_map):
 
