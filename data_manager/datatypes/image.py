6f00c10e01f6962b157b5f7e8da0d37d42dd8743
murch <ryan.c.murchie@gmail.com>
2017-07-25 10:17:56 -0400

    update image loading to use SciKit Image TIFFFile system
    adds support for multidimensional TIFF files

--- data_manager/datatypes/image.py
+++ data_manager/datatypes/image.py
@@ -2,11 +2,14 @@ import matplotlib.pyplot as plt
 
 from data_manager.datatypes.data import Data
 
+import json as json
+
 import logging as logging
 logger = logging.getLogger(__name__)
 
 import scipy as scipy
 import skimage.io as skimage_io
+import skimage.external.tifffile as tff
 
 
 class Image(Data):
@@ -14,11 +17,26 @@ class Image(Data):
     def load(self):
 
         if self.file_ext == "tif":
-            self.data = skimage_io.imread(self.file_name,plugin="tifffile")
+
+
+            #self.data = skimage_io.imread(self.file_name,plugin="tifffile")
+
+            with tff.TiffFile(self.file_name) as tiff:
+                for tiff_page in tiff:
+                    pass
+                    #print(tiff_page.page_name)
+                self.data = tiff.asarray()
+
+                self.metadata = tiff[0].image_description
+                self.info = tiff[0].info()
+
+
+            logging.debug(self.info)
+
 
         self.axes = self.data.shape
-
-        logger.debug("---Image data: Shape - " + str(self.data.shape))
+        print(self.data.shape)
+        logging.debug("---Image data: Shape - " + str(self.data.shape))
 
         return
 
