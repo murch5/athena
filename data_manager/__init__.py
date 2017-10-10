47451f68bc4376c3e8f394c05a9e21194a0d2e57
murch <ryan.c.murchie@gmail.com>
2017-10-10 10:24:34 -0400

    add new import to __init__.py

--- data_manager/__init__.py
+++ data_manager/__init__.py
@@ -1,5 +1,8 @@
 __version__ = "0.1.0"
 
+
 from data_manager.datatypes.data import Data
 from data_manager.datatypes.table import Table
 from data_manager.datatypes.image import Image
+
+from data_manager.data_manager import DataManager
