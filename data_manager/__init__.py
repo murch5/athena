276d02fd8838fd75ecdfc9ba092a6e4b6b2be25b
murch <ryan.c.murchie@gmail.com>
2018-03-13 00:33:45 -0400

    migrated data collection class from old plotanalyze

--- data_manager/__init__.py
+++ data_manager/__init__.py
@@ -6,3 +6,4 @@ from data_manager.datatypes.table import Table
 from data_manager.datatypes.image import Image
 
 from data_manager.data_manager import DataManager
+from data_manager.data_collection import DataCollection
