99ce0345e99cef012d56536d225a74f92b46fa23
murch <ryan.c.murchie@gmail.com>
2018-03-13 00:32:54 -0400

    add datatype map to __init__.py

--- data_manager/datatypes/__init__.py
+++ data_manager/datatypes/__init__.py
@@ -3,3 +3,7 @@ from data_manager.datatypes.table import Table
 from data_manager.datatypes.image import Image
 from data_manager.datatypes.GFF3 import GFF3
 
+datatype_map = {
+    "text/csv": "Table",
+    "image/tiff": "Image"
+}
